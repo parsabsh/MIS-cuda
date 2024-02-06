dd632030b114c718714640b67f7570327fa88a37
Parsa Bashari <m.p.bashari@gmail.com>
2024-02-06 17:42:10 +0330

    clean: documenting the code and remove commented codes

--- helpers.cu
+++ helpers.cu
@@ -1,45 +1,13 @@
 #include <stdio.h>
-// #include "helpers.h"
+#include "helpers.cuh"
 
-// struct Lock {
+dim3 calculateGridDim(int n) {
+    if (n < 65535)
+        return dim3(n, 1); 
+    return dim3(65535, (n + 65535 - 1) / 65535);
+}
 
-//     int *d_state;
-
-//     // --- Constructor
-//     Lock(void) {
-//         int h_state = 0;                                        // --- Host side lock state initializer
-//         cudaMalloc((void **)&d_state, sizeof(int));  // --- Allocate device side lock state
-//         cudaMemcpy(d_state, &h_state, sizeof(int), cudaMemcpyHostToDevice); // --- Initialize device side lock state
-//     }
-
-//     // --- Destructor
-//     __host__ __device__ ~Lock(void) { 
-// #if !defined(__CUDACC__)
-//         cudaFree(d_state); 
-// #else
-
-// #endif  
-//     }
-
-//     // --- Lock function
-//     __device__ void lock(void) { while (atomicCAS(d_state, 0, 1) != 0); }
-
-//     // --- Unlock function
-//     __device__ void unlock(void) { atomicExch(d_state, 0); }
-// };
-
-typedef struct Vertex {
-    int degree;
-    int *Neighbors;
-} Vertex;
-
-typedef struct Graph {
-    int n;
-    int max_degree;
-    Vertex *V;
-} Graph;
-
-Graph* deep_copy_to_device(Graph *graph) {
+void deep_copy_to_device(Graph *graph, Graph **graph_dev) {
     Graph *dev_graph;
     cudaMalloc(&dev_graph, sizeof(Graph));
     cudaMemcpy(dev_graph, graph, sizeof(Graph), cudaMemcpyHostToDevice);
@@ -56,9 +24,8 @@ Graph* deep_copy_to_device(Graph *graph) {
         cudaMalloc(&neighbors, graph->max_degree * sizeof(int));
         cudaMemcpy(neighbors, graph->V[i].Neighbors, graph->max_degree * sizeof(int), cudaMemcpyHostToDevice);
         cudaMemcpy(&(v[i].Neighbors), &neighbors, sizeof(int *), cudaMemcpyHostToDevice);
-        printf("hello %d\n", i);
     }
-    return dev_graph;
+    *graph_dev = dev_graph;
 }
 
 
@@ -77,10 +44,8 @@ Graph* new_graph_host(int n, int max_degree) {
 void add_undirected_edge(Graph *graph, int i, int j) {
     graph->V[i].Neighbors[graph->V[i].degree] = j;
     graph->V[i].degree = graph->V[i].degree + 1;
-    // graph->V[i].Neighbors = (int *) realloc(graph->V[j].Neighbors, graph->V[j].degree * sizeof(int));
     graph->V[j].Neighbors[graph->V[j].degree] = i;
     graph->V[j].degree = graph->V[j].degree + 1;
-    // graph->V[j].Neighbors = (int *) realloc(graph->V[j].Neighbors, graph->V[j].degree * sizeof(int));
 }
 
 __global__ void print_graph_dev(Graph* graph) {
@@ -93,53 +58,8 @@ __global__ void print_graph_dev(Graph* graph) {
         }
         printf("]\n");
     }
-}
+} 
 
-// __global__ void print_graph_dev(Lock lock, Graph* graph) {
-//     int tid = blockIdx.x * blockDim.x + threadIdx.x;
-//     if (tid < graph->n) {
-//         Vertex v = graph->V[tid];
-//         lock.lock();
-//         printf("Vertex %d with degree %d: [ ", tid, v.degree);
-//         for (int j = 0; j < v.degree; j++) {
-//             printf("%d ", v.Neighbors[j]);
-//         }
-//         printf("]\n");
-//         lock.unlock();
-//     }
-// }
-
-__global__ void print_graph_dev(Graph* graph, unsigned int* locks) {
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tid < graph->n) {
-        Vertex v = graph->V[tid];
-        bool leaveLoop = false;
-        while (!leaveLoop) {
-            if (atomicExch(&(locks[tid]), 1u) == 0u) {
-                printf("Vertex %d with degree %d: [ ", tid, v.degree);
-                for (int j = 0; j < v.degree; j++) {
-                    printf("%d ", v.Neighbors[j]);
-                }
-                printf("]\n");
-                leaveLoop = true;
-                atomicExch(&(locks[tid]),0u);
-            }
-        } 
-        
-    }
-}
-
-__global__ void k_testLocking(unsigned int* locks, int n) {
-    int id = threadIdx.x % n;
-    bool leaveLoop = false;
-    while (!leaveLoop) {
-        if (atomicExch(&(locks[id]), 1u) == 0u) {
-            //critical section
-            leaveLoop = true;
-            atomicExch(&(locks[id]),0u);
-        }
-    } 
-}
 
 void print_graph(Graph graph) {
     for (int i = 0; i < graph.n; i++) {
@@ -152,9 +72,17 @@ void print_graph(Graph graph) {
     }
 }
 
-// CUDA kernel for the maximalIndependentSet algorithm
-__global__ void maximalIndependentSetKernel(const Graph* G, int* Flags, int* V) {
-    size_t v = blockIdx.x * blockDim.x + threadIdx.x;
+void print_mis(int* Flags, int n) {
+    printf("MIS set: [");
+    for (int i = 0; i < n; i++) {
+        if (Flags[i] == 1)
+            printf(" %d", i);
+    }
+    printf(" ]\n");
+}
+
+__global__ void maximalIndependentSet(const Graph* G, int* Flags, int* V) {
+    size_t v = blockIdx.x * gridDim.y + blockIdx.y;
 
     while (v < G->n) {
         if (Flags[v]) break;
@@ -170,7 +98,6 @@ __global__ void maximalIndependentSetKernel(const Graph* G, int* Flags, int* V) 
                 }
             }
             if (k == G->V[v].degree) {
-                printf("hi %d\n",v);
                 // Win on self and neighbors, fill flags
                 Flags[v] = 1;
                 for (size_t j = 0; j < G->V[v].degree; j++) {
@@ -189,50 +116,6 @@ __global__ void maximalIndependentSetKernel(const Graph* G, int* Flags, int* V) 
                     }
                 }
             }
-        } 
-        // v += gridDim.x * blockDim.x;
-        // printf("%d : %d\n",v, Flags[v]);
+        }
     }
 }
-
-
-__global__ void maximalIndependentSet(const Graph* G, int* lock, int* Flags) {
-    size_t v = blockIdx.x * blockDim.x + threadIdx.x;
-
-    while (atomicCAS(lock, 0, 1) != 0);
-    printf("hi %d\n", v);
-    bool f = true;
-    for (size_t i = 0; i < G->V[v].degree; i++)
-    {
-        int ngh = G->V->Neighbors[i];
-        if (Flags[ngh] == 1)
-            f = false; 
-    }
-    if (!f)
-        Flags[v] = 0;
-    
-    atomicExch(lock, 0);
-    // *lock = 0;
-}
-
-__global__ void maximalIndependentSet(const Graph* G, int* Flags) {
-    size_t v = blockIdx.x * blockDim.x + threadIdx.x;
-
-    // while (atomicCAS(lock, 0, 1) != 0);
-    // printf("hi %d\n", v);
-    bool f = true;
-    for (size_t i = 0; i < G->V[v].degree; i++)
-    {
-        int ngh = G->V->Neighbors[i];
-        if (Flags[ngh] == 1)
-            f = false; 
-    }
-    if (!f)
-        Flags[v] = 1;
-    
-    // atomicExch(lock, 0);
-    // *lock = 0;
-}
-
-// __global__ void find_indepenedent_set(const Graph* G, int* Flags, )
-
---
+++ helpers.cuh
@@ -0,0 +1,37 @@
+typedef struct Vertex {
+    int degree;
+    int *Neighbors;
+} Vertex;
+
+typedef struct Graph {
+    int n;
+    int max_degree;
+    Vertex *V;
+} Graph;
+
+// calculate the grid dimensions based on n and the maximum number of blocks in each grid dimension
+dim3 calculateGridDim(int n);
+
+// perform a deep copy of the data structure Graph, from host to device and set graph_dev pointer to it
+void deep_copy_to_device(Graph *graph, Graph **graph_dev);
+
+// construct a new graph with n vertices and max_degree
+// NOTE: max_degree is by default eqaul to n. But in sparse graphs,
+// knowing the max_degree will increase space efficiency.
+Graph* new_graph_host(int n, int max_degree);
+
+// add an undirected edge between i and j to the graph
+void add_undirected_edge(Graph *graph, int i, int j);
+
+// print a graph
+void print_graph(Graph graph);
+
+
+// print a Maximal Independent Set
+void print_mis(int* Flags, int n);
+
+// CUDA kernel for the maximalIndependentSet algorithm
+__global__ void maximalIndependentSet(const Graph* G, int* Flags, int* V);
+
+// CUDA kernel for printing a graph on the device
+__global__ void print_graph_dev(Graph* graph);
